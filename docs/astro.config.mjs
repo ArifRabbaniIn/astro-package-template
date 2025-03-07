177f336f8d1d78bc7764c3ab0e98da91d345b8a6
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-08 00:57:12 +0530

    docs: add docs with starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'My Awesome Package',
+      description:
+        'Default description',
+      lastUpdated: true,
+      credits: true,
+      social: {
+        github: 'GITHUB_REPOSITORY_URL',
+      },
+      sidebar: [
+        {
+          label: 'Start Here',
+          items: [
+            { label: 'Introduction', slug: 'introduction' },
+          ],
+        },
+      ],
+    }),
+  ],
+});
