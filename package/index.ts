3693a08c8a9f26ada0e6e355c5d673c0cdc271e4
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-10 16:37:48 +0530

    package: add dummy HelloWorld component

--- package/index.ts
+++ package/index.ts
@@ -1,2 +1,6 @@
-// Main entry point of the package
-// Export anything you want to make public
+// Do not write code directly here, instead use the `src` folder!
+// Re-export all the things you want your user to access.
+
+import HelloWorld from './src/HelloWorld.astro';
+
+export { HelloWorld };
